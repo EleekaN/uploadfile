1bdfab4bfd576a07a9a0b14eb50cbd72b3aeb920
Eleeka <eleeka.nongthombam@gmail.com>
2023-11-22 07:40:10 +0000

    updated index and layout.html

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if request.method == "POST":
+        return "file uploaded"
+    else:
+        return render_template("/")
